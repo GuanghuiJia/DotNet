052eaacfbaaa71d271b1e8b7f7fd16f4b4ae4158
guanghuijia-gd <guanghui.jia@greendotcorp.com>
2022-11-07 17:05:02 +0800

    add hello api

---
+++ src/AspNetCoreWebApi/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Hello()
+        {
+            return Ok("hello world");
+        }
+    }
+}
